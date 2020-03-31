dfb2935787ff71158e1a6e9c3ceea44548b809e1
rinex20 <rinex20@me.com>
2020-03-31 16:02:10 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-wget -c http://mirrors.linuxeye.com/oneinstack-full.tar.gz
-tar xzf oneinstack-full.tar.gz
-cd oneinstack
-./install.sh --nginx_option 1 --php_option 5\
+echo "Download Oneinstack full..."
+wget -O oneinstack-full.tar.gz http://mirrors.linuxeye.com/oneinstack-full.tar.gz 
+tar xzf oneinstack-full.tar.gz && cd oneinstack
+chmod +x install.sh && ./install.sh\
+ --nginx_option 1 --php_option 5\
  --phpcache_option 1 --phpmyadmin  --db_option 3\
  --dbinstallmethod 1 --dbrootpwd v4RdF92cn
 
-"Downloading appbus source code"
+echo "Downloading appbus source code..."
 wget -O appbus.zip https://raw.githubusercontent.com/datagnss/appbus/master/raw/appbus.zip
-
 unzip appbus.zip -d /data/wwwroot/default
 
 cd /data/wwwroot/default
