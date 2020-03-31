a4a93354e145fc207c1e09e1a07dcae4afbd83df
rinex20 <rinex20@me.com>
2020-03-31 17:03:18 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-echo "Download Oneinstack full..."
-wget -O oneinstack-full.tar.gz http://mirrors.linuxeye.com/oneinstack-full.tar.gz\ 
-  && tar xzf oneinstack-full.tar.gz && cd oneinstack && chmod +x install.sh && ./install.sh\
- --nginx_option 1 --php_option 5\
- --phpcache_option 1 --phpmyadmin  --db_option 3\
- --dbinstallmethod 1 --dbrootpwd v4RdF92cn
-
-echo "Downloading appbus source code..."
-wget -O appbus.zip https://raw.githubusercontent.com/datagnss/appbus/master/raw/appbus.zip\
-  && unzip appbus.zip -d /data/wwwroot/default\
-  && cd /data/wwwroot/default\
-  && mv Install/apktool /usr/bin && chmod 0755 /usr/bin/apktool\ 
-  && mv Install/apktool.jar /usr/bin && chmod 0755 /usr/bin/apktool.jar\
-  && mv Install/aapt /usr/bin && chmod 0755 /usr/bin/aapt
-
 chown -R www:www /data/wwwroot/default/ && \
 chown -R www:www /data/wwwroot/default/upload 
 
@@ -36,4 +21,3 @@ cat>/etc/nginx/conf.d/appbus-8081.conf<<EOF
 >"server_name localhost;"
 >EOF
 
-service nginx restart
