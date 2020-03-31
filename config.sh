782756ab51bbecf9d1b7428084f2f45201e106fd
rinex20 <rinex20@me.com>
2020-03-31 21:43:02 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -21,13 +21,16 @@ chmod -R 0755 /data/wwwroot/default/upload
 echo "config php options(remove exec security, modify upload fileszie)"
 sed -n '/disable_functions/p' /usr/local/php/etc/php.ini | sed 's/exec,//g' 
 sed -n '/upload_max_filesize/p' /usr/local/php/etc/php.ini | sed 's/upload_max_filesize/#upload_max_filesize/g'
-sed -n '/post_max_size/p' /usr/local/php/etc/php.ini | sed 's/upload_max_filesize/#post_max_size/g'
+sed -n '/post_max_size/p' /usr/local/php/etc/php.ini | sed 's/post_max_size/#post_max_size/g'
 sed -i '$a upload_max_filesize = 128m' /usr/local/php/etc/php.ini
 sed -i '$a post_max_size = 128m' /usr/local/php/etc/php.ini
 
 
 echo "Create config file for appbus nginx"
+mkdir /etc/nginx/conf.d
+
 touch /etc/nginx/conf.d/appbus-8081.conf
+
 cat>/etc/nginx/conf.d/appbus-8081.conf<<EOF
 >"listen 8081;"
 >"server_name localhost;"
