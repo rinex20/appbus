1ea9e143fdc73985f56901035e636a62c29d0427
rinex20 <rinex20@me.com>
2020-03-31 17:42:53 +0800

    Update oneinstack.sh

--- oneinstack.sh
+++ oneinstack.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-echo "downloading oneinstack..."
-wget -O oneinstack-full.tar.gz http://mirrors.linuxeye.com/oneinstack-full.tar.gz  
-tar xzf oneinstack-full.tar.gz  
+# Download binary file
+ONE_FILE="oneinstack-full.tar.gz"
 
-cd oneinstack
+echo "downloading oneinstack..."
+wget -O /root/oneinstack-full.tar.gz http://mirrors.linuxeye.com/${ONE_FILE} > /dev/null 2>&1  
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to download file: ${ONE_FILE}" && exit 1
+fi
+echo "Download file: ${ONE_FILE} completed"
+
+tar xzf /root/oneinstack-full.tar.gz  
+
+cd /root/oneinstack
 chmod +x install.sh && ./install.sh \
  --nginx_option 1 --php_option 5 \
  --phpcache_option 1 --phpmyadmin  --db_option 3 \
