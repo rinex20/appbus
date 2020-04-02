cd59543735f2961062bdc1a707e5957df5263f5e
rinex20 <rinex20@me.com>
2020-04-02 10:10:19 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -20,13 +20,13 @@ chmod -R 0755 /data/wwwroot/default/upload
 
 # delete exec in the line which contain disable_functions
 echo "config php options(remove exec security, modify upload fileszie)"
-sed -n '/disable_functions/p' ${PHP_INI} | sed 's/,exec//g' 
-sed -n '/upload_max_filesize/p' ${PHP_INI} | sed 's/50/128/g'
-sed -n '/post_max_size/p' ${PHP_INI} | sed 's/100/128/g'
+sed -n '/disable_functions/p' ${PHP_INI} | sed -i 's/,exec//g'  ${PHP_INI}
+sed -n '/upload_max_filesize/p' ${PHP_INI} | sed -i 's/50/128/g'  ${PHP_INI}
+sed -n '/post_max_size/p' ${PHP_INI} | sed -i 's/100/128/g' ${PHP_INI}
 #sed -i '$a upload_max_filesize = 128m' ${PHP_INI}
 #sed -i '$a post_max_size = 128m' ${PHP_INI}
 
 #echo "config nginx.conf"
-#sed -n '/server_name _;/p' ${N_CONF} | sed 's/_/localhost/g'
+#sed -n '/server_name _;/p' ${N_CONF} | sed -i 's/_/localhost/g'  ${N_CONF}
 
 echo "appbus configure finished."
