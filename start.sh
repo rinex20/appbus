925dcc381ed2602b679b186827e8312f6da72c3c
rinex20 <rinex20@me.com>
2020-04-02 10:22:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+chown -R www:www /data/wwwroot/default/ 
+chown -R www:www /data/wwwroot/default/upload 
+
+chmod -R 0755 /data/wwwroot/default
+chmod -R 0755 /data/wwwroot/default/upload
+
 /etc/init.d/php-fpm start
 /etc/init.d/mysqld start
 /etc/init.d/nginx start
