3d8043c0c36d387777c07325880b4bb989a5d4c3
rinex20 <rinex20@me.com>
2020-04-10 12:38:57 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -21,14 +21,14 @@ chmod -R 0755 /data/wwwroot/default
 chmod -R 0755 /data/wwwroot/default/upload
 
 # delete exec in the line which contain disable_functions
-# echo "config php options(remove exec security, modify upload fileszie)"
-# sed -n '/disable_functions/p' ${PHP_INI} | sed -i 's/,exec,/,/g'  ${PHP_INI}
-# sed -n '/upload_max_filesize/p' ${PHP_INI} | sed -i 's/50/128/g'  ${PHP_INI}
-# sed -n '/post_max_size/p' ${PHP_INI} | sed -i 's/100/128/g' ${PHP_INI}
+echo "config php options(remove exec security, modify upload fileszie)"
+sed -n '/disable_functions/p' ${PHP_INI} | sed -i 's/,exec,/,/g'  ${PHP_INI}
+sed -n '/upload_max_filesize/p' ${PHP_INI} | sed -i 's/50/128/g'  ${PHP_INI}
+sed -n '/post_max_size/p' ${PHP_INI} | sed -i 's/100/128/g' ${PHP_INI}
 # sed -i '$a upload_max_filesize = 128m' ${PHP_INI}
 # sed -i '$a post_max_size = 128m' ${PHP_INI}
 
 # echo "config nginx.conf"
-# sed -n '/server_name/p' ${N_CONF} | sed -i 's/server_name _/server_name localhost/g'  ${N_CONF}
+sed -n '/server_name/p' ${N_CONF} | sed -i 's/server_name _/server_name localhost/g'  ${N_CONF}
 
 echo "appbus configure finished."
