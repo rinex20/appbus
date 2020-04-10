f8e8af3fb3d58cf7eabe609d9e84e6b2463920af
rinex20 <rinex20@me.com>
2020-04-10 12:39:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ chown -R www:www /data/wwwroot/default/upload
 chmod -R 0755 /data/wwwroot/default
 chmod -R 0755 /data/wwwroot/default/upload
 
-/etc/init.d/php-fpm start
-/etc/init.d/mysqld start
-/etc/init.d/nginx start
+/etc/init.d/php-fpm start \
+ && /etc/init.d/mysqld start \
+ && /etc/init.d/nginx start
